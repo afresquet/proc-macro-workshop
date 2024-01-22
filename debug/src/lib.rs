77b9cc17747666463cfdbae0a4c08ab7921faffa
Alvaro Fresquet <29437693+afresquet@users.noreply.github.com>
2024-01-22 04:09:43 +0100

    feat(debug): test 04 pass

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,6 +1,8 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Field, LitStr};
+use syn::{
+    parse_macro_input, parse_quote, Data, DeriveInput, Field, GenericParam, Generics, LitStr,
+};
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -10,8 +12,11 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     let fields_debug = fields(&input.data).map(field_debug);
 
+    let generics = add_trait_bounds(input.generics);
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
     quote! {
-        impl std::fmt::Debug for #name {
+        impl #impl_generics std::fmt::Debug for #name #ty_generics #where_clause {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 f.debug_struct(#lit_name)
                     #(#fields_debug)*
@@ -57,6 +62,15 @@ fn field_debug(field: &Field) -> TokenStream {
     let lit_name = LitStr::new(&name.to_string(), name.span());
     match debug_attr(field) {
         Some(debug) => quote! { .field(#lit_name, &format_args!(#debug, &self.#name)) },
-        None => quote! { .field(#lit_name, &self.name) },
+        None => quote! { .field(#lit_name, &self.#name) },
     }
+}
+
+fn add_trait_bounds(mut generics: Generics) -> Generics {
+    for param in &mut generics.params {
+        if let GenericParam::Type(type_param) = param {
+            type_param.bounds.push(parse_quote!(std::fmt::Debug));
+        }
+    }
+    generics
 }
