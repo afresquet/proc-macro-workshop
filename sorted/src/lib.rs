b71e0145461752a345d271d8dccde1b9de4c592e
Alvaro Fresquet <29437693+afresquet@users.noreply.github.com>
2024-01-23 00:43:31 +0100

    feat(sorted): test 01 and 02 pass

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,18 @@
-use proc_macro::TokenStream;
+use proc_macro2::{Span, TokenStream};
+use syn::parse_macro_input;
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+pub fn sorted(
+    _: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as syn::Item);
 
-    unimplemented!()
+    let syn::Item::Enum(_) = input else {
+        return syn::Error::new(Span::call_site(), "expected enum or match expression")
+            .to_compile_error()
+            .into();
+    };
+
+    TokenStream::new().into()
 }
