909d2326b749da82650cef14c27b836ea0318979
Alvaro Fresquet <29437693+afresquet@users.noreply.github.com>
2024-01-22 22:21:28 +0100

    feat(seq): test 04 pass

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,4 +1,5 @@
-use proc_macro2::{Group, Literal, TokenStream, TokenTree};
+use itertools::{Itertools, MultiPeek};
+use proc_macro2::{Group, Ident, Literal, TokenStream, TokenTree};
 use syn::parse_macro_input;
 
 #[derive(Debug)]
@@ -12,12 +13,12 @@ struct Sequence {
 impl syn::parse::Parse for Sequence {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let ident = input.parse()?;
-        input.parse::<syn::Token![in]>()?;
+        let _ = input.parse::<syn::Token![in]>()?; // Discard
         let start = input.parse::<syn::LitInt>()?.base10_parse()?;
-        input.parse::<syn::Token![..]>()?;
+        let _ = input.parse::<syn::Token![..]>()?; // Discard
         let end = input.parse::<syn::LitInt>()?.base10_parse()?;
         let content;
-        syn::braced!(content in input);
+        let _ = syn::braced!(content in input); // Discard
         let content = content.parse()?;
         Ok(Sequence {
             ident,
@@ -36,7 +37,6 @@ pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         end,
         content,
     } = parse_macro_input!(input as Sequence);
-    eprintln!("{content:#?}");
 
     (start..end)
         .map(Literal::usize_unsuffixed)
@@ -50,15 +50,66 @@ fn substitute_target(
     target: &syn::Ident,
     substitution: &Literal,
 ) -> TokenStream {
-    content
-        .into_iter()
-        .map(|tt| match tt {
-            TokenTree::Ident(ref ident) if ident == target => substitution.clone().into(),
+    let mut iter = content.into_iter().multipeek();
+    let mut output = Vec::new();
+
+    while let Some(tt) = iter.next() {
+        let tt = match tt {
+            TokenTree::Ident(ref ident) => {
+                parse_ident(&mut iter, ident, substitution, target).unwrap_or(tt)
+            }
             TokenTree::Group(group) => {
                 let stream = substitute_target(group.stream(), target, substitution);
                 Group::new(group.delimiter(), stream).into()
             }
             tt => tt,
-        })
-        .collect()
+        };
+        output.push(tt);
+    }
+
+    TokenStream::from_iter(output)
+}
+
+fn parse_ident(
+    iter: &mut MultiPeek<proc_macro2::token_stream::IntoIter>,
+    ident: &Ident,
+    substitution: &Literal,
+    target: &Ident,
+) -> Option<TokenTree> {
+    let is_tilde = iter
+        .peek()
+        .is_some_and(|tt| matches!(tt, TokenTree::Punct(p) if p.as_char() == '~'));
+    let is_target = iter
+        .peek()
+        .is_some_and(|tt| matches!(tt, TokenTree::Ident(i) if i == target));
+    let is_prefix = is_tilde && is_target;
+
+    let is_tilde = iter
+        .peek()
+        .is_some_and(|tt| matches!(tt, TokenTree::Punct(p) if p.as_char() == '~'));
+    let is_ident = iter
+        .peek()
+        .is_some_and(|tt| matches!(tt, TokenTree::Ident(_)));
+    let is_suffix = is_tilde && is_ident;
+
+    match (ident, is_prefix, is_suffix) {
+        (prefix, true, true) => {
+            iter.next(); // ~
+            iter.next(); // N
+            iter.next(); // ~
+            let Some(TokenTree::Ident(suffix)) = iter.next() else {
+                unreachable!();
+            };
+            let name = format!("{prefix}{}{suffix}", substitution.clone());
+            Some(syn::Ident::new(&name, ident.span()).into())
+        }
+        (prefix, true, _) => {
+            iter.next(); // ~
+            iter.next(); // N
+            let name = format!("{prefix}{}", substitution.clone());
+            Some(syn::Ident::new(&name, ident.span()).into())
+        }
+        (ident, _, _) if ident == target => Some(substitution.clone().into()),
+        (_, _, _) => None,
+    }
 }
