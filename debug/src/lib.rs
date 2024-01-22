d9a670e5664f86b7a668caae7f3305eb2c482054
Alvaro Fresquet <29437693+afresquet@users.noreply.github.com>
2024-01-22 02:17:43 +0100

    feat(debug): test 02 pass

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,10 +1,42 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Field, LitStr};
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
+    let lit_name = LitStr::new(&name.to_string(), name.span());
 
-    quote! {}.into()
+    let fields_debug = fields(&input.data).map(field_debug);
+
+    quote! {
+        impl std::fmt::Debug for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.debug_struct(#lit_name)
+                    #(#fields_debug)*
+                    .finish()
+            }
+        }
+    }
+    .into()
+}
+
+fn fields(data: &Data) -> impl Iterator<Item = &Field> {
+    match data {
+        Data::Struct(data) => match &data.fields {
+            syn::Fields::Named(fields) => fields.named.iter(),
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    }
+}
+
+fn field_debug(field: &Field) -> TokenStream {
+    let Some(name) = &field.ident else {
+        unimplemented!();
+    };
+    let lit_name = LitStr::new(&name.to_string(), name.span());
+
+    quote! { .field(#lit_name, &self.name) }
 }
