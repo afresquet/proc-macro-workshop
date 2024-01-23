6217718da62c994b59105dd132d0ad15b3041315
Alvaro Fresquet <29437693+afresquet@users.noreply.github.com>
2024-01-23 01:00:18 +0100

    feat(sorted): test 03 pass

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -6,13 +6,40 @@ pub fn sorted(
     _: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let input = parse_macro_input!(input as syn::Item);
+    let ast = input.clone();
+    let ast = parse_macro_input!(ast as syn::Item);
 
-    let syn::Item::Enum(_) = input else {
+    let syn::Item::Enum(item_enum) = ast else {
         return syn::Error::new(Span::call_site(), "expected enum or match expression")
             .to_compile_error()
             .into();
     };
 
-    TokenStream::new().into()
+    if let Err(error) = check_sorted(&item_enum) {
+        return error.into();
+    }
+
+    input
+}
+
+fn check_sorted(item_enum: &syn::ItemEnum) -> Result<(), TokenStream> {
+    let variants = item_enum
+        .variants
+        .iter()
+        .map(|variant| &variant.ident)
+        .collect::<Vec<_>>();
+    let mut sorted_variants = variants.clone();
+    sorted_variants.sort();
+
+    for (actual, expected) in variants.into_iter().zip(sorted_variants.into_iter()) {
+        if actual != expected {
+            return Err(syn::Error::new(
+                expected.span(),
+                format!("{expected} should sort before {actual}"),
+            )
+            .to_compile_error());
+        }
+    }
+
+    Ok(())
 }
